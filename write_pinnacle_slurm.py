e34ed030df9dcf3198480680ab26fad0a50c205d
Regina Cabrera <arcabrer@uark.edu>
2023-03-27 01:15:43 -0500

    assn 04 03/27/23

---
+++ write_pinnacle_slurm.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+
+job_name = print('job name')
+queue = print('queue')
+number_of_nodes = print('number of nodes')
+number_of_processors = print('number of processors')
+walltime = print('walltime')
+
