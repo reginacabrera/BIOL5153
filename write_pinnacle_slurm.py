182ba4eaa61b9cfcc90136dcf1bf03176f3fd200
Regina Cabrera <arcabrer@uark.edu>
2023-04-03 09:40:21 -0500

    script modified in class - ass04 & ass05

--- write_pinnacle_slurm.py
+++ write_pinnacle_slurm.py
@@ -1,23 +1,23 @@
-#! usr/bin/env python3
+#! /usr/bin/env python3
 
-# import modules
+# import modules - always before the code (at the top)
 import argparse
 
-# create ArgumentParser object
-parser = argparse.ArgumentParser(description='This script returns a SLURM script with options')
+# create ArgumentParser object - description calls for an output when help is called
+parser = argparse.ArgumentParser(description='This script returns a SLURM script with options fro the AHPCC cluster')
 
 # add positional (required) arguments; when running the ./file -h the -h will display this message saying how the argument was positioned
 
 # add positional arguments
-parser.add_argument('job', help = 'The name of the SLURM job', type = str)
+parser.add_argument('job_name', help = 'The name of the SLURM job', type= str)
 
 # add optional arguments
 # the default for 'store_true' is ... "False"
-# if 'store_true', then assign 'True' with -v or --verbose
-parser.add_argument('-q', '-queue', help = 'Which queue to utilize', default = 'comp01', type = str)
-parser.add_argument('-nodes', help = 'Number of nodes to utilize', default = 1, type = int)
-parser.add_argument('-processors', help = 'Number of processors to utilize', default = 24, type = int)
-parser.add_argument('-walltime', help = 'Estimated time for the job to be processed', default = 1, type = int)
+# if 'store_true', then assign 'True' with -v or --verbose; the - (dash) is the difference between the optional and the require arguments, and double -- before the variable name
+parser.add_argument('-q', '--queue', help = 'Which queue to utilize (comp01, comp06, comp72)', default = 'comp72', type= str)
+parser.add_argument('-n', '--num_nodes', help = 'Number of nodes to utilize', default = '1', type= int)
+parser.add_argument('-p', '--num_processors', help = 'Number of processors to utilize', default = '24', type= int)
+parser.add_argument('-w', '--walltime', help = 'Estimated time for the job to be processed', default = '72', type= int)
 
 # parse the actual arguments
 # access argument values via 'args' variable
@@ -25,24 +25,18 @@ args = parser.parse_args()
 
 # What I really need here:
 
-job_name = args.job
-queue = args.q
-walltime = args.nodes
-num_nodes = args.processors
-num_processors = args.walltime
-
 # print bash header
 print('#!/bin/bash')
 
 print()
 
 # print SBATCH commands
-print('#SBATCH --job-name=' + job_name)
-print('#SBATCH --partition', queue)
-print('#SBATCH --nodes=' + str(num_nodes))
+print('#SBATCH --job-name=' + args.job_name)
+print('#SBATCH --partition', args.queue)
+print('#SBATCH --nodes=' + str(args.num_nodes))
 print('#SBATCH --qos comp')
-print('#SBATCH --tasks-per-node=' + str(num_processors))
-print('#SBATCH --time=' + str(walltime) + ':00:00')
+print('#SBATCH --tasks-per-node=' + str(args.num_processors))
+print('#SBATCH --time=' + str(args.walltime) + ':00:00')
 print('#SBATCH -o arcabrer_%j.out')
 print('#SBATCH -e arcabrer_%j.err')
 print('#SBATCH --mail-type=ALL')
