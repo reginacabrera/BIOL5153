3d39e08e1cbbbba40cb83b4f6f8b99d62d413697
Regina Cabrera <arcabrer@uark.edu>
2023-04-10 09:28:35 -0500

    Another attempt.

--- covid_genome/parseGFF.py
+++ covid_genome/parseGFF.py
@@ -4,25 +4,22 @@
 import argparse
 
 # create ArgumentParser object - description calls for an output when help is called
-parser = argparse.ArgumentParser(description='This script is to execute a FASTA file')
+parser = argparse.ArgumentParser(description='This script parses a GFF file')
 
 # add positional (required) arguments; when running the ./file -h the -h will display this message saying how the argument was positioned
 
 # add positional arguments
-parser.add_argument('GFF_name', help = 'The name of GFF file', type= str)
-parser.add_argument('FASTA_filename', help = 'The name of FASTA file', type= str)
+parser.add_argument('gff', help = 'The name of GFF file to parse', type= str)
+parser.add_argument('fasta', help = 'The name of FASTA file to parse', type= str)
 
 # parse the actual arguments
 # access argument values via 'args' variable
 args = parser.parse_args()
 
-# What I really need here:
+# What I really need here 
 
-file = covid.gff3
-
-open(file)
-
-file.readlines
-
-close(file)
-
+#Read the gff file):
+with open(args.gff) as x:
+	for line in x: # loop over all the lines in the file
+		print(line)
+	
