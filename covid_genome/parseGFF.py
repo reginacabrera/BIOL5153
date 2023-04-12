c105bbb085490ce72eb025e5500d780c27b860e7
Regina Cabrera <arcabrer@uark.edu>
2023-04-12 09:21:37 -0500

    Class mod for parse file

--- covid_genome/parseGFF.py
+++ covid_genome/parseGFF.py
@@ -15,19 +15,72 @@ parser.add_argument('fasta', help = 'The name of FASTA file to parse', type= str
 # parse the actual arguments
 # access argument values via 'args' variable
 args = parser.parse_args()
-	
+
+
+#WHAT I DID
 #reading the file
-GFF_file = open(args.gff_file, 'r')
-GFF_lines = GFF_file.readlines()
-GFF_file.close()
-GFF_lines.remove(GFF_lines[-1])
+
+#GFF_file = open(args.gff_file, 'r')
+#GFF_lines = GFF_file.readlines()
+#GFF_file.close()
+#GFF_lines.remove(GFF_lines[-1])
 
 # GFF_lines2 = []
-for line in GFF_lines:
-    stripped_line = line.strip()
-    stripped_line2 = stripped_line.split('\t')
-    length = (int(stripped_line2[4]) - int(stripped_line2[3]) + 1)
-    print(stripped_line2[2] + '\t' + str(length))
+
+#for line in GFF_lines:
+ #   stripped_line = line.strip()
+ #   stripped_line2 = stripped_line.split('\t')
+ #   length = (int(stripped_line2[4]) - int(stripped_line2[3]) + 1)
+ #   print(stripped_line2[2] + '\t' + str(length))
     
-    
+# ---- CLASS VERSION
+
+#open the GFF file (with function automatically closes the file and the lines become a list)
+with open (args.gff_file) as beyonce:
+
+#loop over all the lines in the file
+ 	for line in beyonce:
+ 			
+ 		#skip blank lines
+ 		if not line.strip():
+ 			continue #this take us to the next line
+ 		
+ 		#else it's not a blank line
+ 		else: 		
+ 			line = line.strip() #remove the line breaks
+ 		
+ 			#split line on tab character
+ 			columns = line.split('\t')
+			# print(line)
+		
+			#give variable names to the column
+ 			organism = columns[0]
+ 			source = columns[1]
+ 			feature_type = columns[2]
+ 			start = columns[3]
+ 			end = columns[4]
+ 			length = columns[5]
+ 			strand = columns[6]
+ 			attributes = columns[8]
+ 			
+ 			#add the length to column 5
+ 			columns[5] = str(int(end) - int(start) + 1)
+ 			
+ 			new_line = '\t'.join(columns)
+
+ 			print(new_line)
+ 
+ # ./nameofthefile.py gff3_file fasta_file | less -S (the less -S will show it organized by columns)		   
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
     
